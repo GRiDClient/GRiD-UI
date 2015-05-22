048a378575ba2c3db30a22c2168363eb7d7cbd4d
Albioncode <me@albionco.de>
2015-05-22 19:10:31 +0100

    Remove Eclipse files
    Commit docs

--- src/com/codingforcookies/mayaui/src/ui/MayaRender.java
+++
@@ -1,9 +0,0 @@
-package com.codingforcookies.mayaui.src.ui;
-
-/**
- * Common abstract class for render components.
- * @author Stumblinbear
- */
-public abstract interface MayaRender {
-	public abstract void render();
-}
